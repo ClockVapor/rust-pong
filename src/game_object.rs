1a65e7123b4b372d06ed9dd69e799e34ca105194
Nick Lowery <ClockVapor@users.noreply.github.com>
2015-05-02 22:34:35 -0600

    Added better collisions

--- src/game_object.rs
+++ src/game_object.rs
@@ -13,6 +13,23 @@ impl GameObject {
         self.pos[1] - self.size[1]/2.0 < other.pos[1] + other.size[1]/2.0 &&
         self.pos[1] + self.size[1]/2.0 > other.pos[1] - other.size[1]/2.0
     }
+
+    pub fn collision_normal(&self, other: &GameObject) -> Option<[f64; 2]> {
+        if self.intersects(other) {
+            let dx = (self.pos[0] - other.pos[0]).abs();
+            let dy = (self.pos[0] - other.pos[0]).abs();
+
+            if dx <= dy {
+                if self.pos[0] <= other.pos[0] { Some([-1.0, 0.0]) }
+                else { Some([1.0, 0.0]) }
+            } else {
+                if self.pos[1] <= other.pos[1] { Some([0.0, -1.0]) }
+                else { Some([0.0, 1.0]) }
+            }
+        } else {
+            None
+        }
+    }
 }
 
 impl Default for GameObject {
